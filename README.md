15e36bad77e73f02f07e443b07aaddaed33a8e4f
AI-WonYJ <yjason7587@naver.com>
2022-10-15 00:51:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Open-Source-Design
+## 오픈소스기초설계 Git
+* 현재 Git에 관하여 배우는 중입니다.
+
+
+> 내 개발 이력
+>> yolo를 이용한 인원수 카운트 봇
+>> 주식 실시간 가격 확인 프로그램
+
+<hr>
+앞으로 해보고 싶은 것들
++ 개인 서버 구축
++ DB다루는 방법 공부
+
+내 백준 현황
+<img src="http://mazandi.herokuapp.com/api?handle=yjason7587&theme=warm"/>
+<hr/>
---
+++ src/Naver_Stock.py
@@ -0,0 +1,32 @@
+#네이버 금융 삼성전자 데이터 수집
+import requests
+from bs4 import BeautifulSoup
+#함수형태 변환
+def get_bs_obj(com_code):
+    url = "https://finance.naver.com/item/main.nhn?code=" + com_code
+    result = requests.get(url)
+    bs_obj = BeautifulSoup(result.content, "html.parser") #html.parser 로 파이썬에서 쓸 수 있는 형태로 변환
+    return bs_obj
+
+def get_price(com_name, com_code):
+  bs_obj = get_bs_obj(com_code)
+  no_today = bs_obj.find("p", {"class":"no_today"})
+  blind_now = no_today.find("span", {"class":"blind"})
+  print("{0} 현재가\n {1}\n".format(com_name, blind_now.text))
+  return blind_now.text
+
+from datetime import datetime
+import time
+
+while True:
+  now = datetime.now()
+  print("             ", now.time(),"\n")
+  # 삼성전자 005930
+  get_price("삼성전자", "005930")
+
+  # LG전자 066570
+  get_price("LG전자", "066570")
+
+  # 카카오 035720
+  get_price("카카오", "035720")
+  print("\n============================================")
